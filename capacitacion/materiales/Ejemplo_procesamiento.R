7572d6458da48202eec45adb36948df7344c95f6
Sebastian Castillo <castilloclaudiosebastian@gmail.com>
2019-06-05 07:51:46 -0300

    actualizacion de presentación Institucional

--- capacitacion/materiales/Ejemplo_procesamiento.R
+++ capacitacion/materiales/Ejemplo_procesamiento.R
@@ -11,3 +11,13 @@ procesado <- DB_PROD() %>%
   group_by(iep, data_interval_start, data_interval_end) %>% 
   process() %>%
   .$result
+
+# Imputación con Evaluaciones según lógicas del negocio
+
+causas_resueltas <- resultado %>%
+  filter(!is.na(fres)) %>% 
+  mutate(control_intervalo =  fres >= data_interval_start & fres < data_interval_end) %>% 
+  mutate(control_mmp = tres != 0 | is.na(tres)) %>% 
+  mutate(
+    atermino = (fres <= fvenc1 & control_intervalo & control_mmp), 
+    res_luego1venc = (fres > fvenc1 & control_intervalo & control_mmp))
